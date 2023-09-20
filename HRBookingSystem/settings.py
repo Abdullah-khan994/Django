79eec71729846f0dcaa33b7cb3f49c700719e985
Abdullah Khan <khanabdullahkhan948@gmail.com>
2023-09-21 00:28:22 +0500

    first commit

--- HRBookingSystem/settings.py
+++ HRBookingSystem/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'khanabdullahkhan948@gmail.com'
-EMAIL_HOST_PASSWORD = 'uqdwyiwdlprjtuqy'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
